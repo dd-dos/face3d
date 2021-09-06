b264513ef2103f0d9d04b5c3c8ea3ca54e1f9726
dd-dos <dungphamdang99@gmail.com>
2021-09-06 00:23:56 +0000

    Update fitting script

--- fit_300WLP.py
+++ fit_300WLP.py
@@ -34,20 +34,19 @@ if __name__=='__main__':
         original_img = cv2.imread(img_path)
         original_pts = sio.loadmat(pts_path)['pt3d']
 
-        expand_ratio = random.uniform(1., 1.4)
-        img, params = model.generate_3ddfa_params(original_img, original_pts, expand_ratio=expand_ratio)
+        img, params = model.generate_3ddfa_params(original_img, original_pts, False, expand_ratio=1.)
         img_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}.jpg')
         params_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}.mat')
         cv2.imwrite(img_out_path, img)
         sio.savemat(params_out_path, params)
 
-        fliplr_img, fliplr_pts = utils.fliplr_face_landmarks(original_img, original_pts, reverse=False)
-        expand_ratio = random.uniform(1., 1.4)
-        fliplr_img, fliplr_params = model.generate_3ddfa_params(fliplr_img, fliplr_pts, expand_ratio=expand_ratio)
-        fliplr_img_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}_fliplr.jpg')
-        fliplr_params_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}_fliplr.mat')
-        cv2.imwrite(fliplr_img_out_path, fliplr_img)
-        sio.savemat(fliplr_params_out_path, fliplr_params)
+        # fliplr_img, fliplr_pts = utils.fliplr_face_landmarks(original_img, original_pts, reverse=False)
+        # expand_ratio = random.uniform(1., 1.4)
+        # fliplr_img, fliplr_params = model.generate_3ddfa_params(fliplr_img, fliplr_pts, expand_ratio=expand_ratio)
+        # fliplr_img_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}_fliplr.jpg')
+        # fliplr_params_out_path = os.path.join('300WLP_3ddfa/300WLP_3ddfa-verified', f'{name}_fliplr.mat')
+        # cv2.imwrite(fliplr_img_out_path, fliplr_img)
+        # sio.savemat(fliplr_params_out_path, fliplr_params)
 
         # foo_pts = model.reconstruct_vertex(fliplr_img, fliplr_params['params'], de_normalize=False)[:,:2][model.bfm.kpt_ind]
         # for pts in foo_pts:
@@ -67,7 +66,7 @@ if __name__=='__main__':
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
 
-    # with mp.Pool(mp.cpu_count()-2) as p:
+    # with mp.Pool(2) as p:
     #     r = list(tqdm.tqdm(p.imap(task, bag), total=len(bag)))
     for item in tqdm.tqdm(bag):
         task(item)
