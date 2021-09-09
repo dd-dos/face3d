7c0355dc18955e094a3ba4becc7f220e64e679c3
dd-dos <dungphamdang99@gmail.com>
2021-09-09 01:44:37 +0000

    Fix typo

--- face3d/morphable_model/morphabel_model.py
+++ face3d/morphable_model/morphabel_model.py
@@ -88,15 +88,6 @@ class MorphabelModel(object):
 
         return ep
     
-    def generate_params(self):
-        random_params = np.random.normal(
-            loc=self.params_mean,
-            scale=self.params_std,
-            size=,)
-        )
-
-        return random_params
-
     def generate_vertices(self, shape_para, exp_para):
         '''
         Args:
